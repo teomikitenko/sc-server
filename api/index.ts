60a7b80ec01d3d8cf073d7303f10cbf1b74c34f5
Yevhenii <jenmikitenko@gmail.com>
2024-10-20 13:27:44 +0300

    setup vercel config

--- api/index.ts
+++ api/index.ts
@@ -3,6 +3,6 @@ import { handle } from "@hono/node-server/vercel";
 // eslint-disable-next-line ts/ban-ts-comment
 // @ts-expect-error
 // eslint-disable-next-line antfu/no-import-dist
-import app from "../dist/src/app.js";
+import app from "../dist/src/index.js";
 
 export default handle(app);
