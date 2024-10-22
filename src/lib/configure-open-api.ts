d3adb1e7e9b4ba495a45234105a53ba08a60cf51
Yevhenii <jenmikitenko@gmail.com>
2024-10-22 19:44:59 +0300

    assert json type

--- src/lib/configure-open-api.ts
+++ src/lib/configure-open-api.ts
@@ -1,5 +1,5 @@
 import type { App } from "../../types/types";
-import packageJSON from "../../package.json";
+import packageJSON from "../../package.json" assert { type: "json" };
 
 export default function configureOpenApi(app: App) {
   app.doc("/doc", {
